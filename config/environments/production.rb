7d3556a6fafd0dbd258c997284c29f7166cc2135
David Ladowitz <david@ladowitz.com>
2015-01-22 16:28:56 -0800

    remembering how rubymine commit function works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = {
-    :host => "nutflix.heroku.com"
+    :host => "littlecatlabs.co"
   }
 
   # mailgun through heroku add-on
